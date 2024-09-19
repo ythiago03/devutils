fac581234f07ff63bb1481fe6e942a1895b847be
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-09-19 16:46:53 -0300

    :heavy_plus_sign: install shadcn and tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+
 function App() {
 
   return (
     <>
-    <h1 className="text-6xl font-bold underline">hello world</h1>
+    <h1 className="text-3xl font-bold underline">hello world</h1>
     </>
   )
 }
