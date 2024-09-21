84e25f892d4ab52345d597a46d625477726ab484
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-09-21 13:58:08 -0300

    :sparkles: create card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,38 @@
+import {
+  Card as CardShadcn,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import './Card.scss';
+
+interface CardInterface {
+  card:{
+    title: string,
+    coverUrl: string,
+    description: string
+    badges: string[]
+  }
+}
+
+const Card: React.FC<CardInterface> = ({card}) => {
+  return (
+    <CardShadcn className="card">
+      <CardHeader className="card_header">
+        <CardTitle>{card.title}</CardTitle>
+        <img className="card_banner" src={card.coverUrl} alt={`Cover from api ${card.title}`} />
+      </CardHeader>
+      <CardContent className="card_content">
+        <CardDescription>{card.description}</CardDescription>
+      </CardContent>
+      <CardFooter className="card_footer">
+        {card.badges.map(badge => <Badge>{badge}</Badge>)}
+      </CardFooter>
+    </CardShadcn>
+  )
+}
+
+export default Card
--- src/pages/Public-Apis/PublicApis.tsx
+++ src/pages/Public-Apis/PublicApis.tsx
@@ -1,79 +1,65 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
+import Card from '../../components/Card/Card';
 import './PublicApis.scss';
 
+interface CardInterface {
+  title: string,
+  coverUrl: string,
+  description: string
+  badges: string[]
+}
+
 const PublicApis = () => {
+  const cards = [
+    {
+      title: 'MyAnimeList',
+      coverUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/58/MyAnimeList_-_Full_Text_Logo.jpg',
+      description: 'Anime and manga database and community.',
+      badges: [
+        'Anime',
+        'Cors',
+        '0Auth',
+        'Https'
+      ]
+    },
+    {
+      title: 'Faker',
+      coverUrl: 'https://fakerjs.dev/social-image.png',
+      description: 'Generate massive amounts of fake (but realistic) data for testing and development.',
+      badges: [
+        'Test Data',
+        'Https',
+        'No auth',
+        'Cors'
+      ]
+    },
+    {
+      title: 'Remove.bg',
+      coverUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBrEZCgCJXEskIKv7yvryMv9O_C9-jyxx58A&s',
+      description: 'Image background removal.',
+      badges: [
+        'Image',
+        'Api Key',
+        'Https',
+        'Cors'
+      ]
+    },
+    {
+      title: 'Spotify',
+      coverUrl: 'https://www.scdn.co/i/_global/open-graph-default.png',
+      description: 'View Spotify music catalog, manage users&apos;libraries, get recommendations and more.',
+      badges: [
+        'Music',
+        'Cors',
+        '0Auth',
+        'Https'
+      ]
+    },
+  ]
+  
   return (
     <section>
-      <h1>Public Apis</h1>
       <div className="cards_container">
-        <Card className="card">
-          <CardHeader>
-            <CardTitle>MyAnimeList</CardTitle>
-            <img className="rounded-md object-cover" src="https://upload.wikimedia.org/wikipedia/commons/5/58/MyAnimeList_-_Full_Text_Logo.jpg" alt="" />
-          </CardHeader>
-          <CardContent>
-            <CardDescription>Anime and manga database and community</CardDescription>
-          </CardContent>
-          <CardFooter className="card_footer">
-            <Badge>Anime</Badge>
-            <Badge>Cors</Badge>
-            <Badge>0auth</Badge>
-            <Badge>Http</Badge>
-          </CardFooter>
-        </Card>
-        <Card className="card">
-          <CardHeader>
-            <CardTitle>MyAnimeList</CardTitle>
-            <img className="rounded-md object-cover" src="https://upload.wikimedia.org/wikipedia/commons/5/58/MyAnimeList_-_Full_Text_Logo.jpg" alt="" />
-          </CardHeader>
-          <CardContent>
-            <CardDescription>Anime and manga database and community</CardDescription>
-          </CardContent>
-          <CardFooter className="card_footer">
-            <Badge>Anime</Badge>
-            <Badge>Cors</Badge>
-            <Badge>0auth</Badge>
-            <Badge>Http</Badge>
-          </CardFooter>
-        </Card>
-        <Card className="card">
-          <CardHeader>
-            <CardTitle>MyAnimeList</CardTitle>
-            <img className="rounded-md object-cover" src="https://upload.wikimedia.org/wikipedia/commons/5/58/MyAnimeList_-_Full_Text_Logo.jpg" alt="" />
-          </CardHeader>
-          <CardContent>
-            <CardDescription>Anime and manga database and community</CardDescription>
-          </CardContent>
-          <CardFooter className="card_footer">
-            <Badge>Anime</Badge>
-            <Badge>Cors</Badge>
-            <Badge>0auth</Badge>
-            <Badge>Http</Badge>
-          </CardFooter>
-        </Card>
-        <Card className="card">
-          <CardHeader>
-            <CardTitle>MyAnimeList</CardTitle>
-            <img className="rounded-md object-cover" src="https://upload.wikimedia.org/wikipedia/commons/5/58/MyAnimeList_-_Full_Text_Logo.jpg" alt="" />
-          </CardHeader>
-          <CardContent>
-            <CardDescription>Anime and manga database and community</CardDescription>
-          </CardContent>
-          <CardFooter className="card_footer">
-            <Badge>Anime</Badge>
-            <Badge>Cors</Badge>
-            <Badge>0auth</Badge>
-            <Badge>Http</Badge>
-          </CardFooter>
-        </Card>
+        {cards.map((card: CardInterface) => <Card card={card}/>)}
       </div>
     </section>
   )
