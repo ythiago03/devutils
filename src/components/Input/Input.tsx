03588fd91a0829fc57f59f3a137eea2208e692ef
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-12-02 19:30:24 -0300

    :hammer: refactor: change html input div into an input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,40 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
+import { Input as InputShadcn } from "../ui/input";
+import { Label } from "../ui/label";
+
+import './Input.scss'
+
+interface InputProps {
+  label: string
+  value: string,
+  copyToClipboard: () => void
+}
+
+const Input: React.FC<InputProps> = ({label, value, copyToClipboard}) => {
+  
+  return (
+    <div className='input_container'>
+    <Label htmlFor={`input-${value}`}>{label}</Label>
+    <div>
+      <InputShadcn data-testid="person-name" disabled type="text" value={value} id={`input-${value}`}/>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+          <span 
+            onClick={copyToClipboard} 
+            className="material-symbols-outlined"
+          >
+            content_paste
+          </span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Copy to clipboard</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  </div>
+  )
+}
+
+export default Input;
