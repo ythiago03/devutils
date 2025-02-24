44a79934fa6580915ed85900aec3e965e5770adb
Thiago Henrique Campos Fidêncio <thiagohcfidencio@gmail.com>
2025-02-24 19:10:39 -0300

    :sparkles: feat: add controls to password generator

--- src/pages/Password/Password.tsx
+++ src/pages/Password/Password.tsx
@@ -1,41 +1,73 @@
 import { Input } from "@/components/ui/input";
 import "./Password.scss";
 import { Slider } from "@/components/ui/slider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Copy, RotateCcw } from "lucide-react";
+import { toast } from "@/hooks/use-toast";
 
 const Password = () => {
 	const [passwordLenght, setPasswordLenght] = useState<number>(16);
 	const [password, setPassword] = useState<string>("");
+	const [containsUppercase, setContainsUppercase] = useState<boolean>(true);
+	const [containsLowercase, setContainsLowercase] = useState<boolean>(true);
+	const [containsNumbers, setContainsNumbers] = useState<boolean>(true);
+	const [containsSpecials, setContainsSpecials] = useState<boolean>(true);
 
-	const generatePassword = (value: number): void => {
+	const getPassword = (value: number): void => {
 		setPasswordLenght(value);
-		
-		let generatedPassword = teste(value);
-		
-		if()
 
+		const generatedPassword = generatePassword(value);
 
 		setPassword(generatedPassword);
 	};
-	
-	const teste (length: number): string => {
-		const characters =
+
+	useEffect(() => {
+		getPassword(passwordLenght);
+	}, []);
+
+	const generatePassword = (length: number): string => {
+		const characters: string =
 			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+		let formatedCharacters = characters;
+
+		if (!containsNumbers) {
+			formatedCharacters = formatedCharacters.replace(/\d/g, "");
+		}
+
+		if (!containsUppercase) {
+			formatedCharacters = formatedCharacters.replace(/[A-Z]/g, "");
+		}
+
+		if (!containsLowercase) {
+			formatedCharacters = formatedCharacters.replace(/[a-z]/g, "");
+		}
+		if (!containsSpecials) {
+			formatedCharacters = formatedCharacters.replace(/[^a-zA-Z0-9]/g, "");
+		}
+		console.log(formatedCharacters);
+
 		let generatedPassword = "";
 		for (let i = 0; i < length; i++) {
-			const randomIndex = Math.floor(Math.random() * characters.length);
-			generatedPassword += characters[randomIndex];
+			const randomIndex = Math.floor(Math.random() * formatedCharacters.length);
+			generatedPassword += formatedCharacters[randomIndex];
 		}
 		return generatedPassword;
-	}
+	};
+
+	const copyToClipboard = (value: string) => {
+		navigator.clipboard.writeText(value);
+		toast({
+			title: "Password copied to clipboard",
+		});
+	};
+
 	return (
 		<section className="password_container">
 			<h2>Password Generator</h2>
 
 			<div className="password-input_wrapper">
-				<Input value={password} />
-				<button type="button">Copy</button>
-				<button type="button">Reset</button>
+				<Input className="h-20 text-xl text-center" value={password} />
 			</div>
 
 			<div className="password-config_wrapper">
@@ -43,11 +75,47 @@ const Password = () => {
 				<p>{passwordLenght}</p>
 				<Slider
 					value={[passwordLenght]}
-					onValueChange={(e) => generatePassword(e[0])}
+					onValueChange={(e) => getPassword(e[0])}
 					min={4}
 					max={100}
 					step={1}
 				/>
+			</div>
+			<div className="modifiers">
+				<Button
+					onClick={() => setContainsUppercase((prev) => !prev)}
+					variant={containsUppercase ? "secondary" : "outline"}
+				>
+					Uppercase
+				</Button>
+				<Button
+					onClick={() => setContainsLowercase((prev) => !prev)}
+					variant={containsLowercase ? "secondary" : "outline"}
+				>
+					Lowercase
+				</Button>
+				<Button
+					onClick={() => setContainsNumbers((prev) => !prev)}
+					variant={containsNumbers ? "secondary" : "outline"}
+				>
+					Numbers
+				</Button>
+				<Button
+					onClick={() => setContainsSpecials((prev) => !prev)}
+					variant={containsSpecials ? "secondary" : "outline"}
+				>
+					Specials
+				</Button>
+			</div>
+			<div className="controls">
+				<Button onClick={() => copyToClipboard(password)} className="gap-3">
+					<Copy />
+					Copy
+				</Button>
+				<Button onClick={() => getPassword(passwordLenght)} className="gap-3">
+					<RotateCcw />
+					Reset
+				</Button>
 			</div>
 		</section>
 	);
